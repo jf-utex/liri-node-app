a921db3899d5e81795a284f24e79fa19285cb8f5
Jeannie <jflaherty@utexas.edu>
2017-09-03 20:55:09 -0500

    liri omdb to work

---
+++ liri.js
@@ -0,0 +1,65 @@
+//write the code you need to grab the data from keys.js.
+var fs = require("fs");
+var request = require("request");
+var twitter = require("twitter");
+var keys = require("./keys.js");
+var spotify = require("spotify");
+
+// the requested title of movie
+var command = process.argv;
+
+var movieName = "";
+
+
+///TWITTER SEARCH
+//
+// function tweetie(user) {
+//
+//   var client = new twitter({
+//     consumer_key: 'keys.twitterKeys.consumer_key',
+//     consumer_secret: 'keys.twitterKeys.consumer_secret',
+//     access_token_key: 'keys.twitterKeys.access_token_key',
+//     access_token_secret: 'keys.twitterKeys.access_token_secret'
+//   });
+//
+//   var params = {
+//     screen_name: 'nodejs'
+//   };
+//   client.get('statuses/user_timeline', params, function(error, tweets, response) {
+//     if (!error) {
+//       for (i = 0; i < tweets.length; i++) {
+//         console.log(i);
+//       }
+//       console.log(tweets);
+//     }
+//   });
+
+
+  ///MOVIE SEARCH ON OMDB
+
+  //3 because node is 0, filename is 1, command is 2 and title is 3
+  for (var i = 2; i < command.length; i++) {
+    if (i > 2 && i < command.length) {
+    movieName = movieName + "+" + command[i];
+  } else {
+    movieName += command[i];
+  }
+}
+
+var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=40e9cece";
+
+// This line is just to help us debug against the actual URL.
+// console.log(queryUrl);
+
+request(queryUrl, function(error, response, body) {
+
+  // If the request is successful
+  if (!error && response.statusCode === 200) {
+
+    // Parse the body of the site and recover just the imdbRating
+    // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
+    console.log("Release Year: " + JSON.parse(body).Year);
+  }
+});
+
+///SPOTIFY
