54e3ff491d61caec419731bd7eb29cff0e7c910d
Jeannie <jflaherty@utexas.edu>
2017-09-04 10:14:54 -0500

    OMDB working - spotify and twitter not so much

--- liri.js
+++ liri.js
@@ -4,20 +4,53 @@ var request = require("request");
 var twitter = require("twitter");
 var keys = require("./keys.js");
 var spotify = require("spotify");
-
-// the requested title of movie
-var command = process.argv;
+var inquirer = require("inquirer");
 
 var movieName = "";
-///TWITTER SEARCH
+
+///GET USER INPUT///
+
+// var inquirer = require('inquirer');
+inquirer.prompt([{
+    type: "list",
+    message: "Welcome to LIRI!. Choose an option below to get started.",
+    choices: ["Twitter - use this to get the most recent 20 tweets", "Spotify - Search Spotify for your song", "OMDB - Retrieve info about your movie"],
+    name: "choice"
+  }
+
+]).then(function(choice) {
+  if (choice.choice == "Twitter - use this to get the most recent 20 tweets") {
+    myTweets();
+
+  } else if (choice.choice == "Spotify - Search Spotify for your song") {
+    spotifyThisSong();
+
+  } else if (choice.choice == "OMDB - Retrieve info about your movie") {
+    movieThis();
+
+    // } else {
+    //   doWhatItSays();
+    // }
+  }
+});
+
+///TWITTER SEARCH///
 //
-// function tweetie(user) {
+// function myTweets() {
+// inquirer.prompt([{
+//   type: "input",
+//   message: "Please enter a Twitter user.",
+//   name: "twitUser"
 //
-//   var client = new twitter({
-//     consumer_key: 'keys.twitterKeys.consumer_key',
-//     consumer_secret: 'keys.twitterKeys.consumer_secret',
-//     access_token_key: 'keys.twitterKeys.access_token_key',
-//     access_token_secret: 'keys.twitterKeys.access_token_secret'
+// }]).then(function(input){
+// var keys = require("./keys.js");
+//   consumer_key: 'keys.twitterKeys.consumer_key',
+//   consumer_secret: 'keys.twitterKeys.consumer_secret',
+//   access_token_key: 'keys.twitterKeys.access_token_key',
+//   access_token_secret: 'keys.twitterKeys.access_token_secret';
+//
+// }
+//
 //   });
 //
 //   var params = {
@@ -34,46 +67,78 @@ var movieName = "";
 
 
 ///MOVIE SEARCH ON OMDB
+function movieThis() {
+  inquirer.prompt([{
+    type: "input",
+    message: "Please provide a movie title.",
+    name: "movieName"
+  }]).then(function(input) {
+    var request = require("request");
+    var movieName = input.movieName;
+    if (movieName == "") {
+      movieName = "Mr. Nobody";
+    }
 
-//3 because node is 0, filename is 1, command is 2 and title is 3
-for (var i = 2; i < command.length; i++) {
-  if (i > 2 && i < command.length) {
-    movieName = movieName + "+" + command[i];
-  } else {
-    movieName += command[i];
-  }
-}
+    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short=true&apikey=40e9cece";
+    // console.log(queryUrl);
 
-var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=40e9cece";
+    request(queryUrl, function(error, response, body) {
 
-// This line is just to help us debug against the actual URL.
-// console.log(queryUrl);
+      // If the request is successful
+      if (!error && response.statusCode === 200) {
 
-request(queryUrl, function(error, response, body) {
+        // Parse the body of the site and return useful information in JSON
+        console.log("Movie Title: " + JSON.parse(body).Title);
+        console.log("Release Year: " + JSON.parse(body).Year);
+        console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+        console.log("Plot: " + JSON.parse(body).Plot);
+        console.log("Actors: " + JSON.parse(body).Actors);
+        console.log("Country: " + JSON.parse(body).Country);
+        // console.log("Rotten Tomatoes Rating: " + JSON.parse(body).tomatoRating);
+        console.log("Language: " + JSON.parse(body).Language);
 
-  // If the request is successful
-  if (!error && response.statusCode === 200) {
-
-    // Parse the body of the site and return useful information in JSON
-    console.log("Movie Title: " + JSON.parse(body).Title);
-    console.log("Release Year: " + JSON.parse(body).Year);
-    console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
-    console.log("Plot: " + JSON.parse(body).Plot);
-    console.log("Actors: " + JSON.parse(body).Actors);
-  }
-});
+      }
+    });
+  });
+};
 
 ///SPOTIFY
 
-spotify.search({
-  type: 'track',
-  query: 'dancing in the moonlight'
-}, function(err, data) {
-  if (err) {
-    console.log('Error occurred: ' + err);
-    return;
-  }
-  console.log("The song you chose is: " + data);
-  return;
 
-});
+
+function spotifyThisSong() {
+  inquirer.prompt([{
+    type: "input",
+    message: "Great! Type in a song name:",
+    name: "songName"
+  }]).then(function(input) {
+    var spotify = require("spotify");
+    var songName = input.songName;
+    if (songName == "") {
+      songName = "The Sign";
+    }
+    var params = {
+      type: "track",
+      query: songName,
+      limit: "20"
+    };
+    var dataArr = data.split(",");
+    spotify.search(params, function(err, data) {
+      if (!err) {
+        console.log(
+          "Information for songs named " + songName);
+        for (var i = 0; i < data.tracks.items.length; i++) {
+          console.log(
+            "Artist: " + data.tracks.items[i].artists[0].name +
+            "Album Name: " + data.tracks.items[i].album.name +
+            "Song Name: " + data.tracks.items[i].name +
+            "Preview link for song: " + data.tracks.items[i].preview_url
+          );
+        }
+      } else {
+        console.log(err);
+
+      }
+    });
+  });
+}
