28d2fe637c7ab3b7372251d841fdd79419945909
Jeannie <jflaherty@utexas.edu>
2017-09-09 12:19:57 -0500

    continued work on twitt and spotify

--- liri.js
+++ liri.js
@@ -36,127 +36,130 @@ inquirer.prompt([{
 
 ///TWITTER SEARCH///
 //
-function myTweets() {
-  inquirer.prompt([{
-    type: "input",
-    message: "Please enter a Twitter user.",
-    name: "twitUser"
 
-  }]).then(function(input) {
-      var keys = require("./keys.js");
-        consumer_key: 'jSvelds2jHfrfWFF5xQ7r9pcY',
-        consumer_secret: 'koaXrv3YKg7y7l2BdGg8eaXYoTyXuoA9GjXznQUc9ZOmsw2TV4',
-        access_token_key: '840568585433026563-f0GVKV1mVH7RAdIqve9AhWByprwvNjG',
-        access_token_secret: '6RacfbhZBmg4uuLBqm0tHANhVUtEWlf6bUToG2I3YtP8q';
+var getMyTweets = function() {
+    var client = new Twitter(keys.twitterKeys);
 
-    }
+    var params = {
+      screen_name: "jf2862"
+    };
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+      if (!error) {
+        for (i = 0; i < tweets.length; i++) {
+          console.log(i);
+        }
 
-var params = {
-  screen_name: 'nodejs'
-};
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
-  if (!error) {
-    for (i = 0; i < tweets.length; i++) {
-      console.log(i);
-    }
-    console.log(tweets);
-  }
-});
-  });
-
-///MOVIE SEARCH ON OMDB
-function movieThis() {
-  inquirer.prompt([{
-    type: "input",
-    message: "Please provide a movie title.",
-    name: "movieName"
-  }]).then(function(input) {
-    var request = require("request");
-    var movieName = input.movieName;
-    if (movieName == "") {
-      movieName = "Mr. Nobody";
-    }
-
-    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short=true&apikey=40e9cece";
-    // console.log(queryUrl);
-
-    request(queryUrl, function(error, response, body) {
-
-      // If the request is successful
-      if (!error && response.statusCode === 200) {
-
-        // Parse the body of the site and return useful information in JSON
-        console.log("Movie Title: " + JSON.parse(body).Title);
-        console.log("Release Year: " + JSON.parse(body).Year);
-        console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
-        console.log("Plot: " + JSON.parse(body).Plot);
-        console.log("Actors: " + JSON.parse(body).Actors);
-        console.log("Country: " + JSON.parse(body).Country);
-        // console.log("Rotten Tomatoes Rating: " + JSON.parse(body).tomatoRating);
-        console.log("Language: " + JSON.parse(body).Language);
-
-      }
+        inquirer.prompt([{
+              type: "input",
+              message: "Please enter a Twitter user.",
+              name: "twitUser"
+              console.log(tweets);
+            }
+          }
+        });
     });
-  });
-};
-
-///SPOTIFY
-
-// function spotifyThisSong() {
-//   inquirer.prompt([{
-//       type: "input",
-//       message: "Type in a song name:",
-//       name: "songName"
-//       id: "2e8bc61d36f642fda45edbf936ed4c29",
-//       secret: "615f876bdc7f4704b41d1398e553e83d"
-//     });
-//
-//     spotify.search({
-//       type: "input",
-//       query: 'songName'
-//     }, function(err, data) {
-//       if (err) {
-//         return console.log('Error occurred: ' + err);
-//       }
-//
-//       console.log(data);
-//     });
-//
-//   });
-
-// search: function({ type: "track", query: "songName", limit: 20 }, callback);
-//
-// var queryUrl = "https://api.spotify.com/v1/search" + songName;
-// var dataArr = data.split(",");
-// spotify.search(params, function(err, data) {
-// if (!err) {
-//   console.log(
-//     "Information for songs named " + songName);
-//   for (var i = 0; i < data.tracks.items.length; i++) {
-//     console.log(
-//       "Artist: " + data.tracks.items[i].artists[0].name +
-//       "Album Name: " + data.tracks.items[i].album.name +
-//       "Song Name: " + data.tracks.items[i].name +
-//       "Preview link for song: " + data.tracks.items[i].preview_url
-//     );
-//   }
-// } else {
-//   console.log(err);
-//
-// }
-// });
-// });
 
 
 
-// pseudocode
+    ///MOVIE SEARCH ON OMDB
+    function movieThis() {
+      inquirer.prompt([{
+        type: "input",
+        message: "Please provide a movie title.",
+        name: "movieName"
+      }]).then(function(input) {
+        var request = require("request");
+        var movieName = input.movieName;
+        if (movieName == "") {
+          movieName = "Mr. Nobody";
+        }
 
-//spotify and twitter integration with existing OMDB
-// for spottify, grab title of song provided: return data including artist, album, song name and preview link
-//default to "the sign"
-// for twitter, return last 20 tweets from user
-// define do-what-it-says
-//use fs node package - will take what is inside random txt file and should run ...
-//return spotify for "I want it that way"
+        var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short=true&apikey=40e9cece";
+        // console.log(queryUrl);
 
-//issue is difficulty integrating all three API's
+        request(queryUrl, function(error, response, body) {
+
+          // If the request is successful
+          if (!error && response.statusCode === 200) {
+
+            // Parse the body of the site and return useful information in JSON
+            console.log("Movie Title: " + JSON.parse(body).Title);
+            console.log("Release Year: " + JSON.parse(body).Year);
+            console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+            console.log("Plot: " + JSON.parse(body).Plot);
+            console.log("Actors: " + JSON.parse(body).Actors);
+            console.log("Country: " + JSON.parse(body).Country);
+            // console.log("Rotten Tomatoes Rating: " + JSON.parse(body).tomatoRating);
+            console.log("Language: " + JSON.parse(body).Language);
+
+          }
+        });
+      });
+    };
+
+    ///SPOTIFY
+    var spotify = new Spotify({
+      id: "2e8bc61d36f642fda45edbf936ed4c29",
+      secret: "615f876bdc7f4704b41d1398e553e83d",
+
+    })
+
+    function spotifyThisSong() {
+      inquirer.prompt([{
+          type: "input",
+          message: "Type in a song name:",
+          name: "songName"
+        });
+
+        spotify.search({
+            type: "input",
+            query: songName
+          },
+          function(err, data) {
+            if (err) {
+              return console.log('Error occurred: ' + err);
+              return;
+            }
+            var songs = data.input.items;
+            for (var i = 0; i < data.tracks.items.length; i++) {
+              console.log(
+                "Artist: " + data.tracks.items[i].artists[0].name +
+                "Album Name: " + data.tracks.items[i].album.name +
+                "Song Name: " + data.tracks.items[i].name +
+                "Preview link for song: " + data.tracks.items[i].preview_url
+              );
+              console.log(data);
+            });
+
+        });
+
+      // search: function({ type: "track", query: "songName", limit: 20 }, callback);
+      //
+      // var queryUrl = "https://api.spotify.com/v1/search" + songName;
+      // var dataArr = data.split(",");
+      // spotify.search(params, function(err, data) {
+      // if (!err) {
+      //   console.log(
+      //     "Information for songs named " + songName);
+      //
+      //   }
+      // } else {
+      //   console.log(err);
+      //
+      // }
+      // });
+      // });
+
+
+
+      // pseudocode
+
+      //spotify and twitter integration with existing OMDB
+      // for spottify, grab title of song provided: return data including artist, album, song name and preview link
+      //default to "the sign"
+      // for twitter, return last 20 tweets from user
+      // define do-what-it-says
+      //use fs node package - will take what is inside random txt file and should run ...
+      //return spotify for "I want it that way"
+
+      //issue is difficulty integrating all three API's
