272beb45c2aeba2b31608c66b76c21910f43192e
Jeannie <jflaherty@utexas.edu>
2017-09-25 09:07:31 -0500

    working on fixing spotify

--- keys.js
+++ keys.js
@@ -6,3 +6,8 @@ exports.twitterKeys = {
   access_token_key: '840568585433026563-f0GVKV1mVH7RAdIqve9AhWByprwvNjG',
   access_token_secret: '6RacfbhZBmg4uuLBqm0tHANhVUtEWlf6bUToG2I3YtP8q',
 }
+
+exports.spotifyKeys ={
+  id: "34e84d93de6a4650815e5420e0361fd3",
+ secret: "5162cd8b5cf940f48702dffe096c2acb"
+}
--- liri.js
+++ liri.js
@@ -1,9 +1,9 @@
 //write the code you need to grab the data from keys.js.
 var fs = require("fs");
 var request = require("request");
-var twitter = require("twitter");
+var Twitter = require("twitter");
 var keys = require("./keys.js");
-var spotify = require("node-spotify-api");
+var spotify = require("spotify");
 var inquirer = require("inquirer");
 
 var movieName = "";
@@ -37,119 +37,120 @@ inquirer.prompt([{
 ///TWITTER SEARCH///
 //
 function twitter() {
+  var client = new Twitter(keys.twitterKeys);
+
+  var params = {
+    screen_name: 'jf2862'
+  };
+  client.get("statuses/user_timeline", params, function(error, tweets, response) {
+    if (!error) {
+
+      for (var i = 0; i < tweets.length; i++) {
+        console.log(tweets[i].created_at);
+        console.log("");
+        console.log(tweets[i].text);
+      }
+    }
+  })
+};
+// var request = require("request");
+// var movieName = input.movieName;
+// if (movieName == "") {
+//   movieName = "Mr. Nobody";
+
+function spotifyThisSong() {
   inquirer.prompt([{
     type: "input",
-    message: "Please enter a Twitter user.",
-    name: "twitUser"
+    message: "Type in a song name:",
+    name: "songName"
+  }]).then(function(input) {
+    spotify.search({
+      type: 'track',
+      query: 'songName'
+    }, function(err, data) {
+      if (err) {
+        console.log('Error occurred: ' + err);
+        return;
+      }
+      console.log(data);
+      //     }
+
+      var getArtist = function(artist){
+      return artist.name;
+    };
+
+      //     var getSpotify = function() {
+      //       if (songName === undefined) {
+      //         songName = "What's my name again?";
+      //       }
+    })
+  })
+};
+//       var params = {
+//         type: "input",
+//         query: songName,
+//         limit: 20
+//
+//         } else {
+//
+//           for (var i = 0; i < data.tracks.items.length; i++) {
+//             console.log(
+//               "Artist: " + data.tracks.items[i].artists[0].name +
+//               "Album Name: " + data.tracks.items[i].album.name +
+//               "Song Name: " + data.tracks.items[i].name +
+//               "Preview link for song: " + data.tracks.items[i].preview_url
+//             );
+//           };
+//         };
+//
+//       });
+//     };
+//   });
+// };
+
+///  WORKING  MOVIE SEARCH ON OMDB
+function movieThis() {
+  inquirer.prompt([{
+    type: "input",
+    message: "Please provide a movie title.",
+    name: "movieName"
   }]).then(function(input) {
     var request = require("request");
-    var twitUser = input.twitUser;
-    if (twitUser == "") {
-      twitUser = "jf2862";
+    var movieName = input.movieName;
+    if (movieName == "") {
+      movieName = "Mr. Nobody";
     }
-    var client = new twitter({
-      consumer_key: process.env.TWITTER_CONSUMER_KEY,
-      consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-      bearer_token: process.env.TWITTER_BEARER_TOKEN
+
+    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short=true&apikey=40e9cece";
+    // console.log(queryUrl);
+
+    request(queryUrl, function(error, response, body) {
+
+      // If the request is successful
+      if (!error && response.statusCode === 200) {
+
+        // Parse the body of the site and return useful information in JSON
+        console.log("Movie Title: " + JSON.parse(body).Title);
+        console.log("Release Year: " + JSON.parse(body).Year);
+        console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+        console.log("Plot: " + JSON.parse(body).Plot);
+        console.log("Actors: " + JSON.parse(body).Actors);
+        console.log("Country: " + JSON.parse(body).Country);
+        // console.log("Rotten Tomatoes Rating: " + JSON.parse(body).tomatoRating);
+        console.log("Language: " + JSON.parse(body).Language);
+
+      }
     });
-  });
-  client.post('statuses/update', {
-    status: 'I Love Twitter'
-  }, function(error, tweet, response) {
-    if (error) throw error;
-    console.log(tweet); // Tweet body.
-    console.log(response); // Raw response object.
   });
 };
+// pseudocode
 
-  // ///SPOTIFY
+//spotify and twitter integration with existing OMDB
+// for spottify, grab title of song provided: return data including artist, album, song name and preview link
+//default to "the sign"
+// for twitter, return last 20 tweets from user
+// define do-what-it-says
+//use fs node package - will take what is inside random txt file and should run ...
+//return spotify for "I want it that way"
 
-  function spotifyThisSong() {
-    inquirer.prompt([{
-      type: "input",
-      message: "Type in a song name:",
-      name: "songName"
-    }]).then(function(input) {
-      var request = require("request");
-      var songName = input.songName;
-      if (songName == "") {
-        songName = "The Sign";
-      }
-
-      var getSpotify = function() {
-        var spotify = new Spotify(keys.spotifyKeys);
-        var params = {
-          type: "input",
-          query: songName,
-          limit: 20
-
-        }
-        var queryUrl = "https://api.spotify.com/v1/search" + songName;
-        spotify.search(params, function(err, data) {
-          if (err) {
-            return console.log('Error occurred: ' + err);
-            return;
-
-          } else {
-
-            for (var i = 0; i < data.tracks.items.length; i++) {
-              console.log(
-                "Artist: " + data.tracks.items[i].artists[0].name +
-                "Album Name: " + data.tracks.items[i].album.name +
-                "Song Name: " + data.tracks.items[i].name +
-                "Preview link for song: " + data.tracks.items[i].preview_url
-              );
-            };
-          };
-
-        });
-      };
-    });
-  };
-
-  ///  WORKING  MOVIE SEARCH ON OMDB
-  function movieThis() {
-    inquirer.prompt([{
-      type: "input",
-      message: "Please provide a movie title.",
-      name: "movieName"
-    }]).then(function(input) {
-      var request = require("request");
-      var movieName = input.movieName;
-      if (movieName == "") {
-        movieName = "Mr. Nobody";
-      }
-
-      var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short=true&apikey=40e9cece";
-      // console.log(queryUrl);
-
-      request(queryUrl, function(error, response, body) {
-
-        // If the request is successful
-        if (!error && response.statusCode === 200) {
-
-          // Parse the body of the site and return useful information in JSON
-          console.log("Movie Title: " + JSON.parse(body).Title);
-          console.log("Release Year: " + JSON.parse(body).Year);
-          console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
-          console.log("Plot: " + JSON.parse(body).Plot);
-          console.log("Actors: " + JSON.parse(body).Actors);
-          console.log("Country: " + JSON.parse(body).Country);
-          // console.log("Rotten Tomatoes Rating: " + JSON.parse(body).tomatoRating);
-          console.log("Language: " + JSON.parse(body).Language);
-
-        }
-      });
-    });
-  };
-  // pseudocode
-
-  //spotify and twitter integration with existing OMDB
-  // for spottify, grab title of song provided: return data including artist, album, song name and preview link
-  //default to "the sign"
-  // for twitter, return last 20 tweets from user
-  // define do-what-it-says
-  //use fs node package - will take what is inside random txt file and should run ...
-  //return spotify for "I want it that way"
-
-  //issue is difficulty integrating all three API's
+//issue is difficulty integrating all three API's
